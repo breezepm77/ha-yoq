fa5b0721998f2fe7009d3a79fadc41a8999eb2b2
rofiqusmonov613 <rofiqusmonov613@gmail.com>
2021-09-12 16:53:39 +0500

    ha-yoq

---
+++ script.js
@@ -0,0 +1,28 @@
+const elForm = document.querySelector('#form');
+const elInput = document.querySelector('#input');
+const elCheckboxFirst = document.querySelector('.checkbox1');
+const elCheckboxSec = document.querySelector('.checkbox2');
+const elYes = document.querySelector('#yes');
+const elNo = document.querySelector('#no');
+
+elForm.addEventListener('keyup', function(){
+    if(elInput.value >= 5 && elInput.value < 30){
+        elYes.classList.add('yes')
+    }
+
+    if(elCheckboxFirst.checked &&  elInput.value >= 5 && elInput.value < 30){
+        elYes.classList.remove('yes')
+        elNo.classList.add('no')
+    }
+
+    if(elCheckboxFirst.checked && elCheckboxSec.checked && elInput.value >= 5 && elInput.value < 30){
+        elNo.classList.remove('no')
+        elYes.classList.add('yes')
+    }
+
+    if(elInput.value < 5 && elCheckboxSec.checked){
+        elNo.classList.add('no')
+        // elYes.classList.add('yes')
+    }
+
+})
